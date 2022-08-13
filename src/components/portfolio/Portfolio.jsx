7f074f842f72143f1ba3f392b92ff03303b3df03
Ismail <i180516@nu.edu.pk>
2022-08-14 04:43:34 +0500

    My Portfolio

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,10 +1,7 @@
 import './portfolio.css'
-import IMG1 from '../../assets/portfolio1.jpg'
-import IMG2 from '../../assets/portfolio2.jpg'
-import IMG3 from '../../assets/portfolio3.jpg'
-import IMG4 from '../../assets/portfolio4.jpg'
-import IMG5 from '../../assets/portfolio5.png'
-import IMG6 from '../../assets/portfolio6.jpg'
+import IMG1 from '../../assets/proshop.png'
+import IMG2 from '../../assets/houseMarketPlace.png'
+import IMG3 from '../../assets/restApi.png'
 
 const data = [
   {
@@ -13,41 +10,25 @@ const data = [
     title: 'ProShop',
     github: 'https://github.com/Ismail-butt/proshop',
     demo: 'https://prostore101.herokuapp.com/',
+    description:
+      'An E-commerce store for digital items like Camera, Wireless headphones, PlayStation etc with PayPal integration.',
   },
   {
     id: 2,
     image: IMG2,
-    title: 'House Market Place',
+    title: 'House MarketPlace',
     github: 'https://github.com/Ismail-butt/house-marketplace',
     demo: 'https://house-marketplace-alpha-six.vercel.app/',
+    description:
+      'A property listing site where new places can be added for renting and sale after registration.',
   },
   {
     id: 3,
     image: IMG3,
-    title: 'ProShop',
-    github: 'https://github.com/Ismail-butt/proshop',
-    demo: 'https://prostore101.herokuapp.com/',
-  },
-  {
-    id: 4,
-    image: IMG4,
-    title: 'ProShop',
-    github: 'https://github.com/Ismail-butt/proshop',
-    demo: 'https://prostore101.herokuapp.com/',
-  },
-  {
-    id: 5,
-    image: IMG5,
-    title: 'ProShop',
-    github: 'https://github.com/Ismail-butt/proshop',
-    demo: 'https://prostore101.herokuapp.com/',
-  },
-  {
-    id: 6,
-    image: IMG6,
-    title: 'ProShop',
-    github: 'https://github.com/Ismail-butt/proshop',
-    demo: 'https://prostore101.herokuapp.com/',
+    title: 'Rest Api',
+    github: 'https://github.com/Ismail-butt/rest-api',
+    description:
+      'REST API Build with Node.js, Express, TypeScript, MongoDB &amp; Zod. Testing Express REST API With Jest & Supertest.',
   },
 ]
 
@@ -58,13 +39,16 @@ const Portfolio = () => {
       <h2>Portfolio</h2>
 
       <div className='container portfolio__container'>
-        {data.map(({ id, image, title, github, demo }) => {
+        {data.map(({ id, image, title, github, demo, description }) => {
           return (
             <article key={id} className='portfolio__item'>
               <div className='portfolio__item-image'>
                 <img src={image} alt={title} />
               </div>
               <h3>{title}</h3>
+
+              <p>{description}</p>
+
               <div className='portfolio__item-cta'>
                 <a
                   href={github}
@@ -76,7 +60,9 @@ const Portfolio = () => {
                 </a>
                 <a
                   href={demo}
-                  className='btn btn-primary'
+                  className={
+                    demo ? 'btn btn-primary' : 'btn btn-primary disabled'
+                  }
                   rel='noreferrer'
                   target='_blank'
                 >
