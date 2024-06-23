87d5e82fa47aa92db11c44ee5a2f8a0ec4b34a77
Muhammad Ismail <muhammad.ismail@ocloudsolutions.net>
2024-06-23 14:46:18 -0800

    chore: updated content

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,17 +1,17 @@
-import React from 'react'
-import Resume from "../../assets/Ismail's Resume.pdf"
+import React from "react";
+import Resume from "../../assets/Ismail's Resume.pdf";
 
 const CTA = () => {
   return (
-    <div className='cta'>
-      <a href={Resume} download className='btn'>
+    <div className="cta">
+      <a href={Resume} download className="btn disabled">
         Download CV
       </a>
-      <a href='#contact' className='btn btn-primary'>
+      <a href="#contact" className="btn btn-primary">
         Let's Talk
       </a>
     </div>
-  )
-}
+  );
+};
 
-export default CTA
+export default CTA;
